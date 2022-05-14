40d1cbf721306a34dba42905f01373c16216483a
ABA Games <cs8k-cyu@asahi-net.or.jp>
2022-05-14 10:22:53 +0900

    Commit sample code

--- samples/committed_src/matchall.js
+++ samples/committed_src/matchall.js
@@ -1,2 +1,7 @@
 const regexp = /t(e)(st(\d?))/g;
 const str = "test1test2";
+
+const array = [...str.matchAll(regexp)];
+
+console.log(array[0]);
+// expected output: Array ["test1", "e", "st1", "1"]
